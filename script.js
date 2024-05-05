c6cb032f18c262ef6217a45ee07a5d7c96275567
Michael LaFond <Mike24mn@gmail.com>
2024-05-05 10:48:31 -0500

    NEW COPY USING TEMPLATE RATHER THEN FORK - Added data-test criteria to index.html to ensure tests run correctly

---
+++ script.js
@@ -0,0 +1,100 @@
+// So far, unnecessary object REMEMBER TO DELETE IF NOT USED
+/* const employees = [
+    firstName = '',
+    lastName = '',
+    idNumber = '',
+    jobTitle = '',
+    annualSalary = ''
+] 
+*/
+
+function handleSubmit(event) {
+    console.log('handle submit works...')
+    event.preventDefault()
+    let newTotal = 0
+    console.log("initialValue is: ", newTotal);
+    let firstNameInput = document.getElementById('firstName').value
+    let lastNameInput = document.getElementById('lastName').value
+    let idInput = document.getElementById('identity').value
+    let titleInput = document.getElementById('title').value
+    let annualSalaryInput = document.getElementById('annualSalary').value
+    let messageOutput = document.getElementById('messages')
+    let formattedSalary = formatter.format(parseFloat(annualSalaryInput))
+
+    console.log(`incoming information: ${firstNameInput},  ${lastNameInput}, ${idInput}, ${annualSalaryInput}`)
+    if (firstNameInput.length > 0 && lastNameInput.length > 0 && idInput.length > 0 && titleInput.length > 0 && annualSalaryInput.length > 0) {
+        {
+            let tableInfusion = document.getElementById("containerLocation")
+            tableInfusion.innerHTML += `<tr>
+    <td>${firstNameInput}</td>
+    <td>${lastNameInput}</td>
+    <td>${idInput}</td>
+    <td>${titleInput}</td>
+    <td>${formattedSalary}</td>
+    <td><button onClick="deleteSubmission(event)">Delete</button></td>
+    </tr>
+  `
+            updateTotalMonthly(parseFloat(annualSalaryInput)); // Update our current totalVal within the <span> HTML element by converting the current annualSalary table submission to an integer base 10 value, then passes that numerical value to our updateTotalMonthly function that performs basic addition to update our <span>! 
+        }
+
+    }
+    else {
+        messageOutput.innerHTML = "* Missing information, please ensure all fields are filled out prior to submission"
+    }
+    document.getElementById("firstName").value = ""
+    document.getElementById("lastName").value = ""
+    document.getElementById("identity").value = ""
+    document.getElementById("title").value = ""
+    document.getElementById("annualSalary").value = ""
+}
+
+// End handleSubmit function
+
+function updateTotalMonthly(amount) {
+    let totalElement = document.getElementById('totalVal')
+    let currentTotal = parseFloat(totalElement.textContent)
+    let employeeMonthlySal = amount / 12
+    let newTotal = currentTotal + employeeMonthlySal
+
+    totalElement.textContent = newTotal.toFixed(2) 
+
+    checkTotalBudget(newTotal)
+}
+
+function checkTotalBudget(total) {
+    let footerElement = document.querySelector('footer') // check first element within footer
+    console.log("footerElement is: ", footerElement);
+    console.log(document.querySelector('footer'));
+    if (total > 20000) {
+        footerElement.classList.add('over-budget')
+        console.log("Warning: Over Budget");
+    }
+    else {
+        footerElement.classList.remove('over-budget')
+    }
+}
+
+//End addEmployeeInfo function
+
+function deleteSubmission(event) {
+    console.log('deleteSubmission works')
+    console.log(event)
+    let thisColumn = event.target.parentElement.parentElement
+    let deleteTableRow = event.target.parentElement.parentElement // Targets parents parent element, meaning entire table row in this scenario!
+    thisColumn.remove()
+}
+
+//End deleteSubmission function
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+}); // Number formatter for the footers Total Monthly value
+const formattedNumberTest = formatter.format(1222222222) // Testing formatter functionality
+console.log(formattedNumberTest);
+
+///* This application should have a form with five inputs that collect a 
+//new employee's *first name, last name, ID number, job title, annual salary*.
+//
